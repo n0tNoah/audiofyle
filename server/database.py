c26fe9ec87293853028da562460d6272f4d12500
NotNoah <vishal>
2021-02-21 13:44:12 +0530

    correction in db_config

--- server/database.py
+++ server/database.py
@@ -3,11 +3,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .settings import SETTINGS
-SQLALCHEMY_DATABASE_URL = "postgresql://odoo12:odoo12@localhost/test_audio"
 db_config=SETTINGS.get('db')
 SQLALCHEMY_DATABASE_URL = db_config.get('type',"postgresql")+\
-    "://"+db_config.get('user',"odoo12")+\
-        ":"+db_config.get('password',"odoo12")+\
+    "://"+db_config.get('user',"vishal")+\
+        ":"+db_config.get('password',":(((((")+\
             "@"+db_config.get('host',"localhost")+\
             "/"+db_config.get('db_name',"test_audio")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
